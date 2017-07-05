6f7ec72d3e3b489121a7e11ab66a2b5c70f3d201
Bill Hefty <billhefty@gmail.com>
2017-07-04 21:33:46 -0500

    remove unused code

--- app/app.js
+++ app/app.js
@@ -22,61 +22,13 @@ import configureStore from './store'
 // Import CSS reset and Global Styles
 import './global-styles'
 
-// Import root routes
-// import createRoutes from './routes'
+// Import routes
 import RouteConfig from './routes'
-// const childRoutes = createRoutes()
 
 // Create redux store with history
 const history = createHistory()
 const initialState = {}
 const store = configureStore(initialState, history)
-
-// const history = syncHistoryWithStore(browserHistory, store, {
-//   selectLocationState: makeSelectLocationState()
-// })
-
-// Set up the router, wrapping all Routes in the App component
-// const rootRoute = {
-//   component: App,
-//   childRoutes: createRoutes(store)
-// }
-
-// const render = () => {
-//   ReactDOM.render(
-//     <Provider store={store}>
-//       <Router
-//         history={history}
-//         routes={rootRoute}
-//         // render={} // TODO: Add scroll to top with reac-router v4
-//       />
-//     </Provider>,
-//     document.getElementById('app')
-//   )
-// }
-
-// const routes = [
-//   {
-//     path: '/',
-//     component: NotFoundPage
-//   }
-// ]
-
-// const RouteWithSubRoutes = (route) => (
-//   <Route path={route.path} render={props => (
-//     <route.component {...props} routes={route.routes} />
-//   )} />
-// )
-
-// const RouteConfig = () => (
-//   <Router>
-//     <div>
-//       {routes.map((route, i) => (
-//         <RouteWithSubRoutes key={i} {...route} />
-//       ))}
-//     </div>
-//   </Router>
-// )
 
 const render = () => {
   ReactDOM.render(
@@ -88,7 +40,6 @@ const render = () => {
 }
 // render()
 if (module.hot) {
-  // render()
   module.hot.accept('containers/App', () => {
     render()
   })
