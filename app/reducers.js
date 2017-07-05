6221fee41cc5f37cbc9cf8bcc36035ed4339d0d2
Bill Hefty <billhefty@gmail.com>
2017-07-04 21:28:01 -0500

    remove commented code

--- app/reducers.js
+++ app/reducers.js
@@ -1,24 +1,6 @@
 import { combineReducers } from 'redux-immutable'
-// import { fromJS } from 'immutable'
 import { routerReducer as routing } from 'react-router-redux'
 import { counterApp } from 'containers/HomePage/reducer'
-
-// Merge route location changes into immutable state
-// const routeInitialState = fromJS({
-//   locationBeforeTransitions: null
-// })
-
-// function routeReducer (state = routeInitialState, action) {
-//   switch (action.type) {
-//     /* istanbul ignore next */
-//     case LOCATION_CHANGE:
-//       return state.merge({
-//         locationBeforeTransitions: action.payload
-//       })
-//     default:
-//       return state
-//   }
-// }
 
 // Creates main reducer with async loaded ones
 export default function createReducer (asyncReducers) {
