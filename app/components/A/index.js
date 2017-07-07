1b899fc194fed5a2d82ef4427af32ef1c1fe231d
Bill Hefty <billhefty@gmail.com>
2017-07-06 20:52:10 -0500

    Add styled anchor component

---
+++ app/components/A/index.js
@@ -0,0 +1,17 @@
+/**
+*
+* A - Styled anchor tag
+*
+*/
+
+import styled from 'styled-components'
+
+const A = styled.a`
+  color: #41addd;
+
+  &:hover {
+    color: #6cc0e5
+  }
+`
+
+export default A
