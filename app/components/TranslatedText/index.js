ab1e4b2ab6ccc1edab7f7b3329a210460acb9634
Bill Hefty <billhefty@gmail.com>
2017-07-08 13:04:33 -0500

    add TranslatedText comp

---
+++ app/components/TranslatedText/index.js
@@ -0,0 +1,37 @@
+/**
+*
+* TranslatedText
+*
+*/
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import LoadingIndicator from 'components/LoadingIndicator'
+
+function TranslatedText ({ loading, error, translation }) {
+  if (loading) {
+    return <LoadingIndicator />
+  }
+
+  if (error !== false) {
+    const ErrorComponent = () => (
+      <p>Something went wrong, please try again!</p>
+    )
+    return <ErrorComponent />
+  }
+
+  if (translation !== false) {
+    return <p>{translation}</p>
+  }
+
+  return null
+}
+
+TranslatedText.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.any,
+  translation: PropTypes.any
+}
+
+export default TranslatedText
---
+++ app/components/TranslatedText/tests/index.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import TranslatedText from '../index'
+import LoadingIndicator from 'components/LoadingIndicator'
+
+describe('<TranslatedText />', () => {
+  it('should render the loading indicator when it is loading', () => {
+    const renderedComponent = shallow(
+      <TranslatedText loading />
+    )
+    expect(renderedComponent.contains(<LoadingIndicator />)).toEqual(true)
+  })
+
+  it('should render an error if loading failed', () => {
+    const renderedComponent = mount(
+      <TranslatedText
+        loading={false}
+        error={{ message: 'Loading failed!' }}
+      />
+    )
+    expect(renderedComponent.text()).toMatch(/Something went wrong/)
+  })
+
+  it('should render the translation if loading was successful', () => {
+    const translation = 'Translated this, we have.'
+    const renderedComponent = shallow(
+      <TranslatedText
+        translation={translation}
+        error={false}
+      />
+    )
+    expect(renderedComponent.contains(<p>{translation}</p>)).toEqual(true)
+  })
+
+  it('should not render anything if nothing interesting is provided', () => {
+    const renderedComponent = shallow(
+      <TranslatedText
+        translation={false}
+        error={false}
+        loading={false}
+      />
+    )
+    expect(renderedComponent.html()).toEqual(null)
+  })
+})
