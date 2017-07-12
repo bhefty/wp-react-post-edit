7448185fa71463b18e527ae6869e2d6ff0693189
Bill Hefty <billhefty@gmail.com>
2017-07-11 21:52:37 -0500

    add testing for footer components

--- app/components/Footer/tests/index.test.js
+++ app/components/Footer/tests/index.test.js
@@ -3,14 +3,20 @@ import { shallow } from 'enzyme'
 
 import A from 'components/A'
 import Footer from '../index'
+import SocialStrip from 'components/SocialStrip'
 
 describe('<Footer />', () => {
   it('should render the credits', () => {
     const renderedComponent = shallow(<Footer />)
     expect(renderedComponent.contains(
       <section>
-        Made by <A href='https://billhefty.com/' target='_blank'>Bill Hefty</A>
+        made by <A href='https://billhefty.com/' target='_blank'>bill hefty</A> &copy; 2017
       </section>
     )).toBe(true)
+  })
+
+  it('should contain <SocialStrip />', () => {
+    const renderedComponent = shallow(<Footer />)
+    expect(renderedComponent.contains(<SocialStrip />)).toBe(true)
   })
 })
--- app/components/SocialStrip/tests/index.test.js
+++ app/components/SocialStrip/tests/index.test.js
@@ -1,10 +1,27 @@
-// import React from 'react'
-// import { shallow } from 'enzyme'
+import React from 'react'
+import { shallow } from 'enzyme'
 
-// import SocialStrip from '../index'
+import SocialStrip from '../index'
+import GitHub from '../GitHub'
+import Twitter from '../Twitter'
+import LinkedIn from '../LinkedIn'
+import Email from '../Email'
 
 describe('<SocialStrip />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false)
+  const renderedComponent = shallow(<SocialStrip />)
+  it('should contain GitHub social link', () => {
+    expect(renderedComponent.contains(<GitHub />))
+  })
+
+  it('should contain Twitter social link', () => {
+    expect(renderedComponent.contains(<Twitter />))
+  })
+
+  it('should contain LinkedIn social link', () => {
+    expect(renderedComponent.contains(<LinkedIn />))
+  })
+
+  it('should contain Email social link', () => {
+    expect(renderedComponent.contains(<Email />))
   })
 })
