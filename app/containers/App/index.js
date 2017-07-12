75c0e4ae6352625a37b722d4752bc72a80255dd8
Bill Hefty <billhefty@gmail.com>
2017-07-11 22:03:51 -0500

    fix min-height of app view

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,7 +12,7 @@ const AppWrapper = styled.div`
   max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
-  min-height: 100%;
+  min-height: calc(100vh - 172px); // min height for App wrapper should account for nav and footer
   padding: 0 16px;
   flex-direction: column;
 `
