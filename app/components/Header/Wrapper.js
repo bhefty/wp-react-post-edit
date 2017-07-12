2023eead0944896a5fcba0d80fca80f24aff49fc
Bill Hefty <billhefty@gmail.com>
2017-07-11 21:11:47 -0500

    style header navbar

---
+++ app/components/Header/Wrapper.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  header {
+    height: 48px;
+
+    img {
+      height: 100%;
+      margin-right: 1em;
+    }
+
+    .pull-right {
+      float: right;
+    }
+  }
+`
--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,21 +1,32 @@
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
 
-import NavBar from './NavBar'
-import HeaderLink from './HeaderLink'
+import Img from 'components/Img'
+import logo from 'assets/logo_white.png'
+import Wrapper from './Wrapper'
 
 class Header extends PureComponent {
   render () {
     return (
-      <header>
-        <NavBar>
-          <HeaderLink to='/'>
-            Home
-          </HeaderLink>
-          <HeaderLink to='/about'>
-            About
-          </HeaderLink>
-        </NavBar>
-      </header>
+      <Wrapper>
+        <header className='sticky'>
+          <label className='drawer-toggle button' htmlFor='navigation-toggle' />
+          <Link to='/' className='logo hidden-sm'>
+            <Img src={logo} alt='React Redux Boilerplate Logo' />
+            <span>React Redux Boilerplate</span>
+          </Link>
+          <Link to='/features' className='button pull-right hidden-sm'>Features</Link>
+          <Link to='/about' className='button pull-right hidden-sm'>About</Link>
+        </header>
+        <input id='navigation-toggle' type='checkbox' />
+        <nav className='drawer hidden-md hidden-lg'>
+          <label htmlFor='navigation-toggle' className='close' />
+          <br />
+          <Link to='/'>Home</Link>
+          <Link to='/features'>Features</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      </Wrapper>
     )
   }
 }
