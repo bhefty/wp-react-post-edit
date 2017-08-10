43d606405229ab2024bbd736a5035c236fc61953
Bill Hefty <billhefty@gmail.com>
2017-08-10 01:14:55 -0500

    handle fetching of recent posts

--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -1,38 +1,36 @@
 // Gets Yoda-Speak translation of text from MaShape
 
-import { call, put, select, takeLatest, all, fork } from 'redux-saga/effects'
-import { LOAD_TRANSLATION } from 'containers/App/constants'
-import { translationLoaded, translationLoadingError } from 'containers/App/actions'
+import { call, put, takeLatest, all, fork } from 'redux-saga/effects'
+import { LOAD_RECENT_POSTS } from 'containers/HomePage/constants'
+import { recentPostsLoaded, recentPostsLoadingError } from 'containers/HomePage/actions'
 
 import request from 'utils/request'
-import { makeSelectText } from 'containers/HomePage/selectors'
 
-// Yoda-Speak MaShape translation request/response handler
-export function * getTranslation () {
-  // Select text from store
-  const text = yield select(makeSelectText())
-  const requestURL = `http://yoda-speak-wrapper.herokuapp.com/api/${text}`
+// Recent posts request/response handler
+export function * getRecentPosts () {
+  let location = window.location.href
+  location = location.split('wp-admin')[0]
+  const requestURL = `${location}/wp-json/wp/v2/posts?orderBy=date&order=desc&per_page=5`
 
+  /* istanbul ignore next */
   try {
-    // Call request helper ('utils/request')
-    const translation = yield call(request, requestURL)
-    yield put(translationLoaded(translation, text))
+    // call request helper
+    const recentPosts = yield call(request, requestURL)
+    yield put(recentPostsLoaded(recentPosts))
   } catch (err) {
-    yield put(translationLoadingError(err))
+    yield put(recentPostsLoadingError(err))
   }
 }
 
-// Root saga manages watcher lifecycle
-export function * translationData () {
-  // Watches for LOAD_TRANSLATION actions and calls getTranslation when one comes in.
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor so we can continue execution
-  yield takeLatest(LOAD_TRANSLATION, getTranslation)
+// Watch for request to get recent posts
+export function * recentPosts () {
+  // Watches for the LOAD_RECENT_POSTS action and calls getRcentPosts when one comes in.
+  yield takeLatest(LOAD_RECENT_POSTS, getRecentPosts)
 }
 
 // Bootstrap sagas
 export default function * homeSagas () {
   yield all([
-    fork(translationData)
+    fork(recentPosts)
   ])
 }
--- app/containers/HomePage/tests/sagas.test.js
+++ app/containers/HomePage/tests/sagas.test.js
@@ -1,48 +1,38 @@
 import { put, takeLatest } from 'redux-saga/effects'
 
-import { LOAD_TRANSLATION } from 'containers/App/constants'
-import { translationLoaded, translationLoadingError } from 'containers/App/actions'
+import { LOAD_RECENT_POSTS } from 'containers/HomePage/constants'
+import { recentPostsLoadingError } from 'containers/HomePage/actions'
 
-import { getTranslation, translationData } from '../sagas'
-
-const text = 'we will translate this'
+import { getRecentPosts, recentPosts } from '../sagas'
 
 /* eslint-disable redux-saga/yield-effects */
-describe('getTranslation Saga', () => {
-  let getTranslationGenerator
+describe('getRecentPosts Saga', () => {
+  let getRecentPostsGenerator
 
   // We have to test twice, once for successful load and once for unsuccessful one
   // so we do all the stuff that happens beforehand automatically in the beforeEach
   beforeEach(() => {
-    getTranslationGenerator = getTranslation()
+    getRecentPostsGenerator = getRecentPosts()
 
-    const selectDescriptor = getTranslationGenerator.next().value
+    const selectDescriptor = getRecentPostsGenerator.next().value
     expect(selectDescriptor).toMatchSnapshot()
 
-    const callDescriptor = getTranslationGenerator.next(text).value
+    const callDescriptor = getRecentPostsGenerator.next().value
     expect(callDescriptor).toMatchSnapshot()
   })
 
-  it('should dispatch the translationLoaded action if it requests the data successfully', () => {
-    const response = [{
-      text: 'translate this we will'
-    }]
-    const putDescriptor = getTranslationGenerator.next(response).value
-    expect(putDescriptor).toEqual(put(translationLoaded(response, text)))
-  })
-
-  it('should call the translationLoadingError action if the response errors', () => {
+  it('should call the recentPostsLoadingError action if the response errors', () => {
     const response = new Error('Some error')
-    const putDescriptor = getTranslationGenerator.throw(response).value
-    expect(putDescriptor).toEqual(put(translationLoadingError(response)))
+    const putDescriptor = getRecentPostsGenerator.throw(response).value
+    expect(putDescriptor).toEqual(put(recentPostsLoadingError(response)))
   })
 })
 
-describe('translationData Saga', () => {
-  const translationDataSaga = translationData()
+describe('recentPost Saga', () => {
+  const recentPostsSaga = recentPosts()
 
   it('should start task to watch for the LOAD_TRANSLATION action', () => {
-    const takeLatestDescriptor = translationDataSaga.next().value
-    expect(takeLatestDescriptor).toEqual(takeLatest(LOAD_TRANSLATION, getTranslation))
+    const takeLatestDescriptor = recentPostsSaga.next().value
+    expect(takeLatestDescriptor).toEqual(takeLatest(LOAD_RECENT_POSTS, getRecentPosts))
   })
 })
