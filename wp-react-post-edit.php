8541a928745a74c2f820975227e3297ec05fe355
Bill Hefty <billhefty@gmail.com>
2017-08-09 19:53:22 -0500

    remove references to widget

--- wp-react-post-edit.php
+++ wp-react-post-edit.php
@@ -7,17 +7,7 @@
 * Author URL:  https://www.billhefty.com
 */
 
-include dirname( __FILE__ ) . '/widget.php';
-
 class WP_React_Post_Edit {
-  static function widgets_init() {
-    register_widget( 'React_Post_Edit_Widget' );
-  }
-
-  static function enqueue_scripts() {
-    wp_enqueue_script( 'wp-react-post-edit', plugins_url( 'build/widget.js', __FILE__ ), array(), 'v0.0.1', true );
-    wp_enqueue_style( 'wp-react-post-edit', plugins_url( 'build/widget.css', __FILE__ ), array(), 'v0.0.1' );
-  }
 
   static function admin_enqueue_scripts() {
     wp_enqueue_script( 'wp-react-post-edit-admin', plugins_url( 'build/admin.js', __FILE__ ), array(), 'v0.0.1', true );
@@ -37,9 +27,9 @@ class WP_React_Post_Edit {
   static function render_editor() {
     echo '<div id="wp-react-post-editor"></div>';
   }
+
 }
 
-add_action( 'widget_init', array( 'WP_React_Post_Edit', 'widgets_init' ) );
 add_action( 'wp_equeue_scripts', array( 'WP_React_Post_Edit', 'enqueue_scripts' ) );
 add_action( 'admin_enqueue_scripts', array( 'WP_React_Post_Edit', 'admin_enqueue_scripts' ) );
 add_action( 'admin_menu', array( 'WP_React_Post_Edit', 'add_admin_page' ) );
