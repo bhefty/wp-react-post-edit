67e18aca0fa2c61fbe15a9ad724344137b458ec7
Bill Hefty <billhefty@gmail.com>
2017-08-10 01:13:47 -0500

    simply render latest 5 posts when button is clicked

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -3,104 +3,41 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-import { makeSelectTranslation, makeSelectLoading, makeSelectError } from 'containers/App/selectors'
-import H1 from 'components/H1'
-import H2 from 'components/H2'
-import TranslatedText from 'components/TranslatedText'
-import CenteredSection from './CenteredSection'
-import Form from './Form'
-import Input from './Input'
-import Section from './Section'
-import { loadTranslation } from '../App/actions'
-import { increment, decrement, changeText } from './actions'
-import { makeSelectCount, makeSelectText } from './selectors'
+import { loadRecentPosts } from './actions'
+import { makeSelectRecentPosts } from './selectors'
 
 export class HomePage extends PureComponent {
   static propTypes = {
-    loading: PropTypes.bool,
-    error: PropTypes.oneOfType([
-      PropTypes.object,
+    recentPosts: PropTypes.oneOfType([
+      PropTypes.array,
       PropTypes.bool
-    ]),
-    translation: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.bool
-    ]),
-    onSubmitForm: PropTypes.func,
-    text: PropTypes.string,
-    onChangeText: PropTypes.func
-  }
-
-  // When initial state text is not null, submit the form to load translation
-  componentDidMount = () => {
-    if (this.props.text && this.props.text.trim().length > 0) {
-      this.props.onSubmitForm()
-    }
+    ])
   }
 
   render () {
-    const { loading, error, translation } = this.props
-    const translationListProps = {
-      loading,
-      error,
-      translation
-    }
-
     return (
-      <article>
+      <div>
         <div>
-          <CenteredSection>
-            <H1>Start your project right away!</H1>
-            <p>A highly scalable, offline-first foundation based on the popular react-boilerplate.</p>
-          </CenteredSection>
-          <Section>
-            <H2>Try me! Redux Saga API Call</H2>
-            <Form onSubmit={this.props.onSubmitForm}>
-              <label htmlFor='text'>
-                Enter some text to see it translated into Yoda-Speak:
-                <br />
-                <Input
-                  id='text'
-                  type='text'
-                  placeholder='You can use this boilerplate to get started right away!'
-                  value={this.props.text}
-                  onChange={this.props.onChangeText}
-                />
-              </label>
-            </Form>
-            <TranslatedText {...translationListProps} />
-          </Section>
+          <button onClick={this.props.onLoadRecentPosts}>Load recent posts</button>
+          {/* istanbul ignore next */ this.props.recentPosts &&
+            this.props.recentPosts.map((post, idx) => {
+              /* istanbul ignore next */
+              return <p key={idx}>{post.title.rendered}</p>
+            })
+          }
         </div>
-        <div>
-          <H2>Simple counter using Redux actions</H2>
-          Current counter value: {this.props.count}
-          <div>
-            <button className='primary' onClick={this.props.onIncrement}>Increment</button>
-            <button className='secondary' onClick={this.props.onDecrement}>Decrement</button>
-          </div>
-        </div>
-      </article>
+      </div>
     )
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  translation: makeSelectTranslation(),
-  text: makeSelectText(),
-  loading: makeSelectLoading(),
-  error: makeSelectError(),
-  count: makeSelectCount()
+  recentPosts: makeSelectRecentPosts()
 })
 
 export function mapDispatchToProps (dispatch) {
   return {
-    onChangeText: (evt) => dispatch(changeText(evt.target.value)),
-    onSubmitForm: (evt) => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault()
-      dispatch(loadTranslation())
-    },
-    onIncrement: () => dispatch(increment()),
-    onDecrement: () => dispatch(decrement())
+    onLoadRecentPosts: () => dispatch(loadRecentPosts())
   }
 }
 
--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,106 +1,28 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
-import TranslatedText from 'components/TranslatedText'
 import { HomePage, mapDispatchToProps } from '../index'
-import { changeText, increment, decrement } from '../actions'
-import { loadTranslation } from '../../App/actions'
+import { loadRecentPosts } from '../actions'
 
 describe('<HomePage />', () => {
-  it('should render the page', () => {
-    const tree = renderer.create(
-      <HomePage />
-    ).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-
-  it('should render the translated text', () => {
-    const renderedComponent = shallow(
-      <HomePage loading error={false} translation={''} />
-    )
-    expect(renderedComponent.contains(<TranslatedText loading error={false} translation={''} />)).toEqual(true)
-  })
-
-  it('should render fetch the translation on mount if text exists', () => {
-    const submitSpy = jest.fn()
-    mount(
-      <HomePage
-        text='Not Empty'
-        onChangeText={() => {}}
-        onSubmitForm={submitSpy}
-      />
-    )
-    expect(submitSpy).toHaveBeenCalled()
+  it('should render to the page', () => {
+    const renderedComponent = shallow(<HomePage />)
+    expect(renderedComponent.find('div').length).toBeGreaterThan(0)
   })
 
   describe('mapDispatchToProps', () => {
-    describe('onChangeText', () => {
+    describe('onLoadRecentPosts', () => {
       it('should be injected', () => {
         const dispatch = jest.fn()
         const result = mapDispatchToProps(dispatch)
-        expect(result.onChangeText).toBeDefined()
+        expect(result.onLoadRecentPosts).toBeDefined()
       })
 
-      it('should dispatch changeText when called', () => {
+      it('should dispatch loadRecentPosts when called', () => {
         const dispatch = jest.fn()
         const result = mapDispatchToProps(dispatch)
-        const text = 'we will translate this'
-        result.onChangeText({ target: { value: text } })
-        expect(dispatch).toHaveBeenCalledWith(changeText(text))
-      })
-    })
-
-    describe('onSubmitForm', () => {
-      it('should be injected', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        expect(result.onSubmitForm).toBeDefined()
-      })
-
-      it('should dispatch loadTranslation when called', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        result.onSubmitForm()
-        expect(dispatch).toHaveBeenCalledWith(loadTranslation())
-      })
-
-      it('should preventDefault if called with event', () => {
-        const preventDefault = jest.fn()
-        const result = mapDispatchToProps(() => {})
-        const evt = { preventDefault }
-        result.onSubmitForm(evt)
-        expect(preventDefault).toHaveBeenCalledWith()
-      })
-    })
-
-    describe('onIncrement', () => {
-      it('should be injected', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        expect(result.onIncrement).toBeDefined()
-      })
-
-      it('should dispatch increment when called', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        result.onIncrement()
-        expect(dispatch).toHaveBeenCalledWith(increment())
-      })
-    })
-
-    describe('onDecrement', () => {
-      it('should be injected', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        expect(result.onDecrement).toBeDefined()
-      })
-
-      it('should dispatch decrement when called', () => {
-        const dispatch = jest.fn()
-        const result = mapDispatchToProps(dispatch)
-        result.onDecrement()
-        expect(dispatch).toHaveBeenCalledWith(decrement())
+        result.onLoadRecentPosts()
+        expect(dispatch).toHaveBeenCalledWith(loadRecentPosts())
       })
     })
   })
