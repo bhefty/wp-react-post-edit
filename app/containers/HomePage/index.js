f8d34bae2ff9a12fea3d7a124dbdc95fecb6c3bf
Bill Hefty <billhefty@gmail.com>
2017-08-10 20:08:55 -0500

    layout rough position for future components

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,6 +5,9 @@ import { createStructuredSelector } from 'reselect'
 
 import { loadRecentPosts } from './actions'
 import { makeSelectRecentPosts } from './selectors'
+import H1 from 'components/H1'
+import H2 from 'components/H2'
+import H3 from 'components/H3'
 
 export class HomePage extends PureComponent {
   static propTypes = {
@@ -16,16 +19,35 @@ export class HomePage extends PureComponent {
 
   render () {
     return (
-      <div>
-        <div>
-          <button onClick={this.props.onLoadRecentPosts}>Load recent posts</button>
-          {/* istanbul ignore next */ this.props.recentPosts &&
-            this.props.recentPosts.map((post, idx) => {
-              /* istanbul ignore next */
-              return <p key={idx}>{post.title.rendered}</p>
-            })
-          }
+      <div className='row'>
+        <div className='col-md-7'>
+          <H1>Quick Editor</H1>
+          <p>Update and change the title of one of your recent posts quickly. Don’t need it anymore? Feel free to delete it!</p>
+          <div className='row'>
+            <div className='col-md-offset-1 col-md-6'>
+              <H2>Recent Posts</H2>
+              {/* istanbul ignore next */ this.props.recentPosts &&
+                this.props.recentPosts.map((post, idx) => {
+                  /* istanbul ignore next */
+                  return <p key={idx}>{post.title.rendered}</p>
+                })
+              }
+            </div>
+          </div>
         </div>
+        <div className='col-md-offset-2 col-md-3'>
+          <H3>Help</H3>
+          <ul>
+            <li>Click the title text to edit it</li>
+            <li>Click Save when finished editing</li>
+            <li>Click Cancel to discard changes without saving</li>
+            <li>Click Delete Post to discard the entire post</li>
+          </ul>
+          <footer>
+            Made by Bill Hefty
+          </footer>
+        </div>
+        <button onClick={this.props.onLoadRecentPosts}>Load recent posts</button>
       </div>
     )
   }
