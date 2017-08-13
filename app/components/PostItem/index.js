c1820d8280672b41bf0ac3931e8a7f72a3e78c2b
Bill Hefty <billhefty@gmail.com>
2017-08-13 18:51:33 -0500

    add delete button

--- app/components/PostItem/index.js
+++ app/components/PostItem/index.js
@@ -9,19 +9,36 @@ import PropTypes from 'prop-types'
 
 import Wrapper from './Wrapper'
 import Title from './Title'
+import request from 'utils/request'
 
 export class PostItem extends Component {
   constructor (props) {
     super()
     this.select = this.select.bind(this)
+    this.delete = this.delete.bind(this)
   }
   /* istanbul ignore next */
   select () {
     // Explicitly select the text input using the raw DOM API
     this.titleInput.select()
   }
+
+  /* istanbul ignore next */
+  delete () {
+    let location = window.location.href
+    location = location.split('wp-admin')[0]
+    const requestURL = `${location}/wp-json/wp/v2/posts/1`
+    request(requestURL, {
+      method: 'DELETE',
+      headers: {
+        'X-WP-Nonce': window.wpApiSettings.nonce
+      },
+      credentials: 'same-origin'
+    })
+      .then((result) => console.log(result))
+  }
   render () {
-    const { title } = this.props
+    const { title, postId } = this.props
     return (
       <Wrapper className='row'>
         <Title
@@ -30,14 +47,16 @@ export class PostItem extends Component {
           innerRef={(input) => { this.titleInput = input }}
         />
         <button onClick={this.select} style={{ backgroundColor: 'lightblue', color: 'white' }} className='col-sm-12 col-md-6'>Edit Title</button>
-        <button style={{ backgroundColor: 'red', color: 'white' }} className='col-sm-12 col-md-6'>Delete Post</button>
+        <button onClick={/* istanbul ignore next */() => this.props.onDeletePost(postId)} style={{ backgroundColor: 'red', color: 'white' }} className='col-sm-12 col-md-6'>Delete Post</button>
       </Wrapper>
     )
   }
 }
 
 PostItem.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  postId: PropTypes.string.isRequired,
+  onDeletePost: PropTypes.func
 }
 
 export default PostItem
--- app/components/PostItem/tests/index.test.js
+++ app/components/PostItem/tests/index.test.js
@@ -6,8 +6,9 @@ import PostItem from '../index'
 describe('<PostItem />', () => {
   it('should render an input field', () => {
     const title = 'test'
+    const postId = '1'
     const renderedComponent = mount(
-      <PostItem title={title} />
+      <PostItem title={title} postId={postId} />
     )
     expect(renderedComponent.find('input').length).toEqual(1)
   })
