bd631395658be1be44669245b6ea4be734e678a2
Bill Hefty <billhefty@gmail.com>
2017-08-14 22:02:15 -0500

    add FeaturedImage component

---
+++ app/components/FeaturedImage/tests/index.test.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import { mount } from 'enzyme'
+
+import FeaturedImage from '../index'
+
+describe('<FeaturedImage />', () => {
+  const renderedComponent = mount(
+    <FeaturedImage
+      src='https://test.jpg'
+      alt='test'
+      openEditImage={jest.fn()}
+    />
+  )
+  it('should contain an image', () => {
+    expect(renderedComponent.find('Img').length).toEqual(1)
+  })
+
+  it('should contain button', () => {
+    expect(renderedComponent.find('button').length).toEqual(1)
+  })
+})
