bc939603a71008cbb4f2c93927323fc8e15479f3
Bill Hefty <billhefty@gmail.com>
2017-08-14 22:08:04 -0500

    use FeaturedImage and ChangeFeaturedImage component views

--- app/components/PostItem/index.js
+++ app/components/PostItem/index.js
@@ -7,13 +7,16 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
+import FeaturedImage from 'components/FeaturedImage'
+import ChangeFeaturedImage from 'components/ChangeFeaturedImage'
 import Wrapper from './Wrapper'
 import Title from './Title'
 
 export class PostItem extends PureComponent {
   state = {
     edit: false,
-    editTitleText: this.props.title
+    editTitleText: this.props.title,
+    imageModalOpen: false
   }
 
   editTitle = () => {
@@ -24,15 +27,38 @@ export class PostItem extends PureComponent {
     this.setState({ editTitleText: evt.target.value })
   }
 
+  openEditImage = () => {
+    this.setState({ imageModalOpen: true })
+  }
+
+  closeEditImage = () => {
+    this.setState({ imageModalOpen: false })
+  }
+
   changeTitle = (evt, id) => {
     if (evt !== undefined && evt.preventDefault) evt.preventDefault()
     this.props.onChangeTitle(id, this.state.editTitleText)
   }
 
   render () {
-    const { title, postId } = this.props
+    const { title, postId, featuredMedia } = this.props
     return (
       <Wrapper className='row'>
+        {this.state.imageModalOpen &&
+          <ChangeFeaturedImage
+            isOpen={this.state.imageModalOpen}
+            closeModal={this.closeEditImage}
+            onRequestClose={this.state.closeEditImage}
+            postId={postId}
+          />
+        }
+        {featuredMedia &&
+          <FeaturedImage
+            src={featuredMedia}
+            alt={title}
+            openEditImage={this.openEditImage}
+          />
+        }
         <form onSubmit={(evt) => this.changeTitle(evt, postId)} >
           {this.state.edit
             ? <Title className='col-sm-12' value={this.state.editTitleText} onChange={this.onEdit.bind(this)} />
